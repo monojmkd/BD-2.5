c8bfc56244a5f5972878783349b23b31c0b2b3e7
Monoj Kumar Das <49304400+monojmkd@users.noreply.github.com>
2025-01-21 15:15:58 +0530

    adding resolve path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { resolve } = require('path');
 const app = express();
 const port = 3000;
 // app.use(express.static('public'));
