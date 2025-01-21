03a29ee888aed7eefb4721160efb73eec041370e
Monoj Kumar Das <49304400+monojmkd@users.noreply.github.com>
2025-01-21 15:29:52 +0530

    using different port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { resolve } = require('path');
 const app = express();
-const port = 3000;
+const port = 3010;
 // app.use(express.static('static'));
 app.use(cors());
 let products = [
